2564d103750746dbe9703491a0c03d1a96d3c964
Christian Guevara <christianjguevara@hotmail.com>
2021-07-27 15:28:09 -0400

    Se agrega routes al proyecto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
 import Header from './shared/layout/components/Header';
 
+import Home from './pages/Home';
+
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+
+        <Redirect from="*" to="/404" />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/shared/layout/components/Header.jsx
+++ src/shared/layout/components/Header.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles/Header.css';
 import platziVideoLogo from '../../../assets/images/logo-platzi-video-BW2.png';
 
+import userIcon from '../../../assets/images/user-icon.png';
+
 export default function Header() {
   return (
-    <header>
-      <img src={platziVideoLogo} alt="logo Platzi Video" />
+    <header className="header">
+      <img
+        className="header__img"
+        src={platziVideoLogo}
+        alt="logo Platzi Video"
+      />
+
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          <img src={userIcon} alt="user" />
+          <p>perfil</p>
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Cuenta</Link>
+          </li>
+          <li>
+            <Link to="/">Cerrar Sesion</Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
