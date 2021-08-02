b4766fd05cb269926e3d2f977e2dc83066a7ab14
Christian Guevara <chrisdb150@gmail.com>
2021-08-02 17:39:13 -0400

    se agrega pagina 404 con animacion

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Layout from '../shared/layout/components/Layout';
+import './styles/NotFound.css';
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Not found</h1>
-    </div>
+    <Layout
+      className="not-found"
+      classNameHeader="background-header--not-found"
+      footerBackground="background-footer--not-found"
+    >
+      <section className="main--not-found">
+        <h2 className="animated wobble">404</h2>
+        <p>Pagina no encontrada</p>
+      </section>
+    </Layout>
   );
 }
